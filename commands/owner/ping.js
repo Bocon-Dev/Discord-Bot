cae674b12e33da8895a893b1cba60c160408fd0d
DarkInk <rstonehouse66@gmail.com>
2021-06-05 13:49:00 -0500

    Removing a file, and adding ping.js

---
+++ commands/owner/ping.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'ping',
+    description: "Get the ping",
+    usage: "",
+    category: "",
+    aliases: [],
+    run: async (client, message, args, config) => {
+        let embed = new Discord.MessageEmbed()
+        .setColor("RED")
+        .setTitle("Ping")
+        .setDescription(`API Latency: ${Math.round(client.ws.ping)}ms`)
+        .setTimestamp()
+    message.channel.send(embed)
+    }}
