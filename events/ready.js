d3b7292e1ff187b9e9b89c44003dc547f56bd848
DarkInk <rstonehouse66@gmail.com>
2021-06-06 22:19:07 -0500

    adding auto git puller ||DO NOT RESTART BOT||

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
+
+const config = require('../config/config.json')
+const githubhook = new Discord.WebhookClient(config.gitpullhookid, config.gitpullhooktoken);
 module.exports = {
     type: 'ready',
     async run(client) {
@@ -10,6 +13,25 @@ module.exports = {
 	        `Discord\'s API`,
             `My Prefix Is $`
 	]
+
+// Credit goes to DanBot hosting's discord bot!
+setInterval(() => {
+    exec(`git pull`, (error, stdout) => {
+        let response = (error || stdout);
+        if (!error) {
+            if (response.includes("Already up to date.")) {
+               // pm2stats.send(`[Bot already up to date. No changes since last pull]\n\n\`\`\`\n${response}\n\`\`\``)
+                //console.log('Bot already up to date. No changes since last pull')
+            } else {
+                githubhook.send('**[AUTOMATIC]** \nNew update on GitHub. Pulling. \n\nLogs: \n```' + response + "```" + "\n\n\n**Restarting bot**")
+                setTimeout(() => {
+                  //exec('npx pm2 restart 0')
+                process.exit()
+                }, 1000)
+            };
+        }
+    })
+}, 30000)
 
         setInterval(() => {
             var oss = require('os-utils');
