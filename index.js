cb02733edcd8217d7f43b170af91e2e6555ebf43
DarkInk <rstonehouse66@gmail.com>
2021-06-08 02:37:46 -0500

    if this doesn't work I'm going to bed

--- index.js
+++ index.js
@@ -218,8 +218,8 @@ client.on("messageReactionAdd", (reaction, user) => {
     //if(reaction.emoji.name == "❌") return reaction.message.channel.send(`${user.username}${user.discriminator}`)
     let category = reaction.message.guild.channels.cache.find(c => c.id === "850558312952889374" && c.type === "category");
     if (!category) return reaction.message.reply('Please contact a Admin, The category **DarkerInk** Set doesn\'t exist and This is a problem')
-    if (reaction.message.guild.channels.cache.find(channel => channel.name === `${user.username.toLowerCase()}${user.discriminator}`)) return reaction.message.send('You already have a ticket open').then(m => client.setTimeout(() => { if(!m.deleted) m.delete() }, 10000)) && reaction.users.remove(user)
-    if(reaction.emoji.name == "✅" && reaction.message.id == '851713926903103499')
+    if(reaction.emoji.name == "✅" && reaction.message.id == '851713926903103499') {
+        if (reaction.message.guild.channels.cache.find(channel => channel.name === `${user.username.toLowerCase()}${user.discriminator}-dcsup-ticket`)) return reaction.message.channel.send('You already have a ticket open').then(m => client.setTimeout(() => { if(!m.deleted) m.delete() }, 10000)) && reaction.users.remove(user)
         return reaction.message.guild.channels.create(`${user.tag}-dcsup-ticket`, {
             parent: category,
         }).then(c => {
@@ -227,6 +227,7 @@ client.on("messageReactionAdd", (reaction, user) => {
             reaction.users.remove(user);
             reaction.message.channel.send(`<@${user.id}>, You chose Discord Support Please check <#${c.id}> for your ticket`).then(m => client.setTimeout(() => { if(!m.deleted) m.delete() }, 10000))
         })
+    }
     if(reaction.emoji.name == "🌎" && reaction.message.id == '851713926903103499')
         return reaction.message.guild.channels.create(`${user.tag}-ptsup-ticket`, {
             parent: category,
