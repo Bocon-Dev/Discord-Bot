5ceef0aba20818e22df019a922ca5a4cd3c7bd94
DarkInk <rstonehouse66@gmail.com>
2021-06-07 22:35:51 -0500

    removing the guild check to see if that is what is breaking it

--- events/ticket.js
+++ events/ticket.js
@@ -2,9 +2,6 @@ module.exports = {
     type: 'messageReactionAdd',
     run: async (reaction, user) => {
         if (user.bot) return; // If the user was a bot, return.
-        const guild = client.guilds.cache.get('847266614399336498')
-        if (!reaction.guild) return; // If the user was reacting something but not in the guild/server, ignore them.
-       
              if (reaction.emoji.name === "❌") {
        
                 reaction.channel.send('You pressed a :x: Good Job now to this prize "      " See its nothing because all you did was press a X')
