88b949ccce119da2836c225067f234dbcb4837ae
DarkInk <rstonehouse66@gmail.com>
2021-06-07 22:31:07 -0500

    testing dis out as a example

--- events/ticket.js
+++ events/ticket.js
@@ -1,12 +1,12 @@
 module.exports = {
     type: 'messageReactionAdd',
-    run: async (client, reaction, user) => {
-        if(reaction.message.partial) await reaction.message.fetch();
-  if(reaction.partial) await reaction.fetch();
+    run: async (reaction, user) => {
+        if (user.bot) return; // If the user was a bot, return.
+      
+        if (!reaction.message.guild) return; // If the user was reacting something but not in the guild/server, ignore them.
+       
+             if (reaction.emoji.name === "❌") {
+       
+                  message.channel.send('You pressed a :x: Good Job now to this prize "      " See its nothing because all you did was press a X')
+    }}}
 
-  let message = reaction.message;
-  if(!message) return;
-  if(user.bot) return;
-  //if(!reaction.message.id == '851661776037740556') return logs.send('Wrong Message')
-  message.channel.send('Right Message ID')
-    }}
