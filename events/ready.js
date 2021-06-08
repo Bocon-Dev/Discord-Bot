9e79b049368dadfa7f2dac4ca53f31eca734e08f
DarkInk <rstonehouse66@gmail.com>
2021-06-08 01:48:56 -0500

    fixing the message ID

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ const exec = require('child_process').exec;
 module.exports = {
     type: 'ready',
     async run(client) {
-        client.channels.cache.get('850026128353132594').messages.fetch('851710476399804417')
+        client.channels.cache.get('850026128353132594').messages.fetch('851713926903103499')
         console.log(`ready.js has been loaded`);
         let statuses = [
             `Users Talk`,
