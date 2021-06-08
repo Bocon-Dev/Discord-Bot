4d79345c9443f1b001eef436587a753352f1cfd2
DarkInk <rstonehouse66@gmail.com>
2021-06-07 22:34:43 -0500

    tbh idk

--- events/ticket.js
+++ events/ticket.js
@@ -2,11 +2,11 @@ module.exports = {
     type: 'messageReactionAdd',
     run: async (reaction, user) => {
         if (user.bot) return; // If the user was a bot, return.
-      
-        if (!reaction.message.guild) return; // If the user was reacting something but not in the guild/server, ignore them.
+        const guild = client.guilds.cache.get('847266614399336498')
+        if (!reaction.guild) return; // If the user was reacting something but not in the guild/server, ignore them.
        
              if (reaction.emoji.name === "❌") {
        
-                  message.channel.send('You pressed a :x: Good Job now to this prize "      " See its nothing because all you did was press a X')
+                reaction.channel.send('You pressed a :x: Good Job now to this prize "      " See its nothing because all you did was press a X')
     }}}
 
