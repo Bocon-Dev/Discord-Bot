86200e66b1434edb4e7aef85180a554b72383fc6
DarkInk <rstonehouse66@gmail.com>
2021-06-07 21:23:19 -0500

    making a ticket close command UwU

--- commands/util/ticket-close.js
+++ commands/util/ticket-close.js
@@ -9,5 +9,28 @@ module.exports = {
     category: "Ticket",
     aliases: [""],
     run: async (client, message, args, config) => {
-        message.channel.send('Not finished')
+        if (!message.channel.name.includes("-ticket")) return message.channel.send('This is Not a Ticket Please use this command in a ticket');
+            const embed = new Discord.MessageEmbed()
+                .setTitle('Are you sure?')
+                .setDescription('Do you really want to close #' + message.channel.name + '?\n**This will delete __all__ the channel content!**')
+                .setColor('#e74c3c')
+                .setTimestamp()
+                .setFooter('You have 60s to react')
+
+            em = await message.channel.send('Do you really want to close #' + message.channel.name + '? If you want to close this ticket just press ✅')
+            await em.react('✅')
+            await em.react('❌')
+
+            em.awaitReactions(r => ['✅', '❌'].includes(r.emoji.name), {
+                max: 1,
+                time: 60000
+            }).then(async (collected) => {
+                r = collected.first()
+
+                if (r.emoji.name === '✅') {
+                    message.channel.delete()
+                } else {
+                    message.channel.send('Canceled')
+                }
+            })
     }}
