975165058728eb53c110e98d91bfa3a333e085d6
DarkInk <rstonehouse66@gmail.com>
2021-06-07 19:54:08 -0500

    can't add slash commands ;=;

--- index.js
+++ index.js
@@ -210,39 +210,3 @@ client.on("guildMemberUpdate", (oldMember, newMember) => {
         return logs.send(newNick)
     }
 })
-
-client.on('message', async message => {
-	if (!client.application?.owner) await client.application?.fetch();
-
-	if (message.content.toLowerCase() === '!deploy' && message.author.id === client.application?.owner.id) {
-		const data = {
-			name: 'ping',
-			description: 'Replies with Pong!',
-		};
-
-		const command = await client.application?.commands.create(data);
-		console.log(command);
-	}
-});
-
-const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
-
-client.on('interaction', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	if (interaction.commandName === 'ping') {
-		const row = new MessageActionRow()
-			.addComponents(new MessageButton()
-				.setCustomID('primary')
-				.setLabel('primary')
-				.setStyle('PRIMARY'));
-
-		const embed = new MessageEmbed()
-			.setColor('#0099ff')
-			.setTitle('Some title')
-			.setURL('https://discord.js.org/')
-			.setDescription('Some description here');
-
-		await interaction.reply('Pong!', { ephemeral: true, embeds: [embed], components: [row] });
-	}
-});
