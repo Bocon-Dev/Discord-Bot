920c4c9fed86284e10facb2df88092a124b82b65
DarkInk <rstonehouse66@gmail.com>
2021-06-07 22:42:26 -0500

    Cleaning some stuff up

--- events/ticket.js
+++ events/ticket.js
@@ -1,12 +1,9 @@
 module.exports = {
     type: 'messageReactionAdd',
     run: async (reaction, user) => {
-        if (user.bot) return; // If the user was a bot, return.
-        if (reaction.message.partial) {
-            reaction.message.fetch()
-          }
-             if (reaction.emoji.name === "❌") {
-       
-                message.channel.send('You pressed a :x: Good Job now to this prize "      " See its nothing because all you did was press a X')
-    }}}
-
+        if(user.bot) return
+        logs.log("first check");
+  if (reaction.emoji.name === "✅") {
+    logs.log("second check");
+  }
+    }}
