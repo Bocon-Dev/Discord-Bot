856525eafaa897cb8c27eece8af22b54845d4a02
DarkInk <rstonehouse66@gmail.com>
2021-06-08 00:05:09 -0500

    testing this as I need to

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,7 @@ const exec = require('child_process').exec;
 module.exports = {
     type: 'ready',
     async run(client) {
+        client.channels.cache.get('851653751509221436').messages.fetch('851686355834503218')
         console.log(`ready.js has been loaded`);
         let statuses = [
             `Users Talk`,
