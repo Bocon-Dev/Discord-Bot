9c1c6742475c81ab324e6a1809e252d8fdf39906
DarkInk <rstonehouse66@gmail.com>
2021-06-08 20:35:43 -0500

    Update ticket-upgrade

--- commands/util/ticket-upgrade.js
+++ commands/util/ticket-upgrade.js
@@ -9,5 +9,14 @@ module.exports = {
     category: "Ticket",
     aliases: [""],
     run: async (client, message, args, config) => {
-        message.channel.send('Not finished')
+        //return message.channel.send('Not finished')
+        if (!message.channel.name.includes("-ticket")) return message.channel.send('This is Not a Ticket Please use this command in a ticket');
+        let supportrole = message.guild.roles.cache.find(role => role.id === "847600287421431828")
+        message.channel.updateOverwrite(supportrole, {
+            SEND_MESSAGES: false,
+            VIEW_CHANNEL: false,
+            READ_MESSAGE_HISTORY: false,
+            ATTACH_FILES: false,
+        });
+        return message.channel.send('**Ticket Upgraded**')
     }}
