dee43f9991bfe7e116803f6a4e86e1aec19b01b9
DarkInk <rstonehouse66@gmail.com>
2021-06-08 21:52:48 -0500

    fixing this

--- commands/util/ticket-setup.js
+++ commands/util/ticket-setup.js
@@ -11,5 +11,5 @@ module.exports = {
     run: async (client, message, args, config) => {
         //return message.reply('Not finished yet, rember to import quick.db')
         message.channel.send('Bocon Hosting support!\n\nPlease react to the emoji where you need help with.\n\n:white_check_mark: = Discord Support.\n\n:earth_americas: = Pterodactyl Support.\n\n:shopping_cart: = Other Support.')
-.then(m => m.react(':white_check_mark:') && m.react(':earth_americas:') && m.react( ':shopping_cart:'))
+.then(m => m.react('✅') && m.react('🌎') && m.react('🛒'))
     }}
