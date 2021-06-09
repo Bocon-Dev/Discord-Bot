0f4f8c92ad38d6937195997094b21f4972d1d8e7
DarkInk <rstonehouse66@gmail.com>
2021-06-08 21:51:13 -0500

    fixing the channel setup a bit

--- commands/util/ticket-setup.js
+++ commands/util/ticket-setup.js
@@ -9,7 +9,7 @@ module.exports = {
     category: "Ticket",
     aliases: [""],
     run: async (client, message, args, config) => {
-        return message.reply('Not finished yet, rember to import quick.db')
+        //return message.reply('Not finished yet, rember to import quick.db')
         message.channel.send('Bocon Hosting support!\n\nPlease react to the emoji where you need help with.\n\n:white_check_mark: = Discord Support.\n\n:earth_americas: = Pterodactyl Support.\n\n:shopping_cart: = Other Support.')
 .then(m => m.react(':white_check_mark:') && m.react(':earth_americas:') && m.react( ':shopping_cart:'))
     }}
